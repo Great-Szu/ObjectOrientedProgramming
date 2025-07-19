c7e0456291e1083564ddd7fa7f7ea13c2d89e437
Great-Szu <mateusz.chol.net@outlook.com>
2025-07-19 23:18:52 +0200

    Day 2.1, further development of the song file that consists of classes that are supposed to gather data about songs in structuralized form

--- song.py
+++ song.py
@@ -8,15 +8,84 @@ class Song:
     """
 
     def __init__(self, title, artist, duration=0):
-        """ Song init method
-        
-        Args:
-            title (str): Initialises the 'title' attribute
-            artist (Artist): An Artist object representing the song's creator.
-            duration (Optional [int]: Initial value for the 'duration' attribute.
-                Will default to zero if not specified.
-        """
+
         self.title = title
         self.artist = artist
         self.duration = duration
-        
+
+class Album:
+    """ Class to represent an Album, using it's track list
+
+    Attributes:
+        name (str): The name of the album.
+        year (int): The year the album was released.
+        artist (Artist): The artist responsible for the album. If not specified,
+        the artist will default to an artist with the name "Various Artists"
+        tracks (List[Song]: A list of the song on the album.
+
+    Methods:
+        add_song: Used to add a new song to the album's track list.
+    """
+    def __init__(self, name, year, artist=None):
+        self.name = name
+        self.year = year
+        if artist is None:
+            self.artist = Artist("Various Artists")
+        else:
+            self.artist = artist
+        self.tracks = []
+
+
+    def add_song(self, song, position=None):
+        """Adds a song to the track list
+
+        Args:
+            song (Song): A song to add.
+            position (Optional[int]): If specified, the song will be added to that position
+            in the track list - inserting it between other songs if necessary.
+            Otherwise, the song will be added to the end of the list.
+        """
+        if position is None:
+            self.tracks.append(song)
+        else:
+            self.tracks.insert(position, song)
+
+
+class Artist:
+    """ Basic class to store artist details.
+    Attributes:
+        name (str): The name of the artist.
+        albums (List[Album]): A list of the albums by this artist.
+        The list includes only those albums in this collection, it is
+        not an exhaustive lists of the artist's published albums.
+
+    Methods:
+        add_album: Use to add a new album to the artist's albums list
+    """
+
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+
+    def add_album(self, album):
+        """ Add a new album to the list.
+
+        Args:
+            album (Album): Album object to add to the list.
+            If the album is already present, it will be added again ( although this is yet to be implemented).
+        """
+        self.albums.append(album)
+
+def load_data():
+    new_artist = None
+    new_album = None
+    artist_list = []
+
+    with open("files/album.txt", "r") as albums:
+        for line in albums:
+            # data row should consist of (artist, album, year, song)
+            artist_field, album_field, year_field = tuple(line.strip('\n').split('\t'))
+            year_field = int(year_field)
+
+if __name__ == '__main__':
+    load_data()
