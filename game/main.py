1fee7b1eec362e14c96a4f4379306e8a7930e2ec
Great-Szu <mateusz.chol.net@outlook.com>
2025-08-10 12:36:46 +0200

    Day 8, Super classes

--- game/main.py
+++ game/main.py
@@ -1,17 +1,33 @@
-from player import Player
+from enemy import Enemy, Troll, Vampire
 
-tim = Player("Tim")
+ugly_troll = Troll("Grah")
+print("Ugly troll - {}".format(ugly_troll))
 
-print(tim.name)
-print(tim.lives)
-tim.lives -= 1
-print(tim)
+another_troll = Troll("Ug")
+print("Another troll - {}".format(another_troll))
 
-tim.level = 5
-print(tim)
+brother = Troll("Urg")
+print(brother)
 
-tim.level -= 2
-print(tim)
+ugly_troll.grunt()
+another_troll.grunt()
+brother.grunt()
 
-tim.level -= 4
-print(tim)
+# monster = Enemy("Basic enemy")
+# monster.grunt() .grunt() jest funkcją opisaną dla Troll nie dla Enemy. Klasa nadrzędna nie może korzystać zklasy podrzędnej.
+
+brother.take_damage(5)
+print(brother)
+
+vampire1 = Vampire("Vlad")
+print(vampire1)
+vampire1.take_damage(13)
+print(vampire1)
+
+print("-" * 40)
+another_troll.take_damage(30)
+print(another_troll)
+
+while vampire1.alive:
+    vampire1.take_damage(1)
+    print(vampire1)
