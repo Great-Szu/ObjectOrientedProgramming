9405068540924bd42469cfc81c56fdc738e09b4e
Great-Szu <mateusz.chol.net@outlook.com>
2025-08-17 22:16:43 +0200

    Day 11, finishing Object oriented programming. Composition vs aggregation (two ways of composing project with classes). Inheriting as building blocks in which first class is the keystone

--- game/main.py
+++ game/main.py
@@ -1,45 +1,7 @@
-from enemy import Enemy, Troll, Vampire, VampireKing
-import random
+a = 3
+b = "tim"
+x = 1, 2, 3
 
-# ugly_troll = Troll("Grah")
-# print("Ugly troll - {}".format(ugly_troll))
-#
-# another_troll = Troll("Ug")
-# print("Another troll - {}".format(another_troll))
-#
-# brother = Troll("Urg")
-# print(brother)
-#
-# ugly_troll.grunt()
-# another_troll.grunt()
-# brother.grunt()
-#
-# # monster = Enemy("Basic enemy")
-# # monster.grunt() .grunt() jest funkcją opisaną dla Troll nie dla Enemy. Klasa nadrzędna nie może korzystać zklasy podrzędnej.
-#
-# brother.take_damage(5)
-# print(brother)
-#
-# vampire1 = Vampire("Vlad")
-# print(vampire1)
-# vampire1.take_damage(13)
-# print(vampire1)
-#
-# print("-" * 40)
-# another_troll.take_damage(30)
-# print(another_troll)
-#
-# while vampire1.alive:
-#     vampire1.take_damage(1)
-#     print(vampire1)
-#
-# vampire1.lives = 0
-# vampire1.hit_points = 1
-# print(vampire1)
-
-vamp_k = VampireKing("Belzebub")
-print(vamp_k)
-while vamp_k._alive:
-    damage_do_zadania = random.randint(1,600)
-    vamp_k.take_damage(damage_do_zadania)
-    print(vamp_k)
+print(a)
+print(b)
+print(x)
